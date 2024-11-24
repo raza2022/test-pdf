e22ad46b8850916f29ef5af07f33a079534dcefc
Abdul Hameed <raza2022@gmail.com>
2024-11-24 23:40:06 +0500

    testing demo added

--- client/main.js
+++ client/main.js
@@ -3,48 +3,45 @@ import { Template } from 'meteor/templating';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-
+  Session.set('loading', false);
 });
 
 Template.hello.helpers({
+  loading() {
+    return Session.get('loading');
+  },
 
 });
 
 Template.hello.events({
   'click .simple-pdf'(event, instance) {
-    Meteor.call('puppeteerPDF', (error, pdf) => {
-      if(error) {
-        alert("Something went wrong. If this problem persists, please contact customer support.")
-      } else {
-        let base64 = btoa(String.fromCharCode.apply(null,new Uint8Array(pdf)))
-        forceDownload(base64, 'test.pdf')
-      }
-    })
+    Session.set('loading', true);
+    Meteor.call('puppeteerPDF', processResult);
   },
   'click .static-template-pdf'(event, instance) {
-    Meteor.call('templateStaticPDF', (error, pdf) => {
-      if(error) {
-        alert("Something went wrong. If this problem persists, please contact customer support.")
-      } else {
-        let base64 = btoa(String.fromCharCode.apply(null,new Uint8Array(pdf)))
-        forceDownload(base64, 'test.pdf')
-      }
-    })
+    Session.set('loading', true);
+    Meteor.call('templateStaticPDF', processResult);
   },
 
   'click .dynamic-template-pdf'(event, instance) {
-    Meteor.call('templateDynamicPDF', (error, pdf) => {
-      if(error) {
-        alert("Something went wrong. If this problem persists, please contact customer support.")
-      } else {
-        let base64 = btoa(String.fromCharCode.apply(null,new Uint8Array(pdf)))
-        forceDownload(base64, 'test.pdf')
-      }
-    })
+    Session.set('loading', true);
+    Meteor.call('templateDynamicPDF', processResult);
   },
 });
 
 
+function processResult(error, result) {
+  Session.set('loading', false);
+  if(error) {
+    alert("Something went wrong. If this problem persists, please contact customer support.")
+  } else {
+    let base64 = btoa(String.fromCharCode.apply(null,new Uint8Array(result)))
+    forceDownload(base64, 'test.pdf')
+  }
+}
+
+
+// force download for testing only (ignore it)
 function forceDownload(pdf_url, pdf_name) {
   let link = document.createElement('a');
   link.href = `data:image/png;base64,${pdf_url}`;
